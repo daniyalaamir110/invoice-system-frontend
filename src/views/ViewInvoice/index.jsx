1c6e6a6d55c7657909747a50ce3a857ab71af4bf
daniyalaamir110 <daniyal.amir110@gmail.com>
2023-06-10 18:19:55 +0500

    frontend done

--- src/views/ViewInvoice/index.jsx
+++ src/views/ViewInvoice/index.jsx
@@ -1,10 +1,36 @@
+import _ from "lodash";
 import { useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import Button from "../../components/Button";
-import TextInput from "../../components/TextInput";
-import { CheckIcon } from "../../icons";
-import _ from "lodash";
-import tags from "../Tags/tags.json";
+import invoices from "../Invoices/invoices.json";
+
+const DetailItem = ({ label, value }) => {
+  return (
+    <div className="flex flex-row my-4">
+      <div className="w-full block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+        {label}
+      </div>
+      <div className="w-full block mb-2 text-sm text-gray-900 dark:text-white">
+        {value}
+      </div>
+    </div>
+  );
+};
+
+const OrderItem = ({ tag, description, price }) => {
+  return (
+    <div className="flex flex-row my-4">
+      <div className="w-full block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+        {tag}
+      </div>
+      <div className="w-full block mb-2 text-sm text-gray-900 dark:text-white">
+        {description}
+      </div>
+      <div className="block mb-2 text-sm text-gray-900 dark:text-white">
+        ${price}
+      </div>
+    </div>
+  );
+};
 
 const ViewInvoice = () => {
   const navigate = useNavigate();
@@ -13,37 +39,51 @@ const ViewInvoice = () => {
 
   const id = +params.id;
 
-  const tag = _.find(tags, { id });
-
-  const [name, setName] = useState(tag.name);
-  const [price, setPrice] = useState(tag.price);
+  const invoice = _.find(invoices, { id });
 
   const [loading, setLoading] = useState(false);
 
   return (
     <div className="pt-5 w-[50%]">
-      <TextInput
-        label="Tag Name"
-        value={name}
-        onChange={({ target: { value } }) => setName(value)}
+      <h2 className="mb-4 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+        Client Details
+      </h2>
+      <DetailItem label="Name" value="John Doe" />
+      <DetailItem label="Company" value="Quantum Paradigm (Pvt) Ltd." />
+      <DetailItem label="Phone" value="+92 319 1296 169" />
+      <h2 className="mb-4 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+        Order Details
+      </h2>
+      <div className="flex flex-row my-4">
+        <div className="w-full block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+          Tag
+        </div>
+        <div className="w-full block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+          Description
+        </div>
+        <div className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+          Price
+        </div>
+      </div>
+      <OrderItem
+        tag="Nut - Pistachio, Shelled"
+        description="Dummy Description"
+        price="508"
       />
-      <TextInput
-        label="Price ($)"
-        value={price}
-        onChange={({ target: { value } }) => setPrice(value)}
+      <OrderItem
+        tag="Nut - Pistachio, Shelled"
+        description="Dummy Description"
+        price="508"
       />
-      <Button
-        text="Submit"
-        variant="primary"
-        action={() => {
-          setLoading(true);
-          setTimeout(() => {
-            setLoading(false);
-            navigate("/tag");
-          }, 1000);
-        }}
-        loading={loading}
-        icon={<CheckIcon />}
+      <OrderItem
+        tag="Nut - Pistachio, Shelled"
+        description="Dummy Description"
+        price="508"
+      />
+      <OrderItem
+        tag="Nut - Pistachio, Shelled"
+        description="Dummy Description"
+        price="508"
       />
     </div>
   );
